63b5f7364142c1cb804dbdc186ee4f34b6b596ee
wuyonghui <578311720@qq.com>
2017-10-30 21:05:57 +0800

    style
    style

---
+++ 1.6 - style/src/js/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Header from '../components/header';
+import Main from '../components/main';
+import Footer from '../components/footer';
+import Binding from '../components/binding'; // 双向数据绑定
+import StyleComponent from '../components/style'; // 引入样式演示模块
+class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+           info: '这里是 footer',
+           bindingVal: ''
+        } 
+    }
+    changeBinding(str,even){
+        this.setState({
+        bindingVal:{
+                val: even.target.value,
+                str: str
+           }  
+        })
+    }
+    getChildRefs(item){
+        // 获取子节点DOM节点
+        console.log(item.refs.head)
+    }
+    render() {
+        return (
+            <div>
+                <Header bindVal = {this.state.bindingVal}/>
+                <Main/>
+                <StyleComponent />
+                <Binding getRefs={this.getChildRefs}  bing={this.changeBinding.bind(this)/*注意 this 指向，这里要bind*/}/> 
+                <Footer info={this.state.info} />
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <Index/>,
+    document.getElementById('app')
+)
---
+++ 1.6 - style/webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+// const extractCSS = new extractTextPlugin('css/[name]-[chunkhash].bundle.css');
+// const extractLESS = new extractTextPlugin('css/[name]-[chunkhash].bundle.css');
+
+
+const config = {
+    context:__dirname + '/src',
+    entry: './js/index.js',
+	output: { // 出口文件路径，__dirname 指向当前项目根路径。
+		path: __dirname + '/src/js/',
+		filename: 'bundle/index.bundle.js'
+	},
+	plugins: [
+		
+	],
+	module: {
+		rules: [ 
+			{
+				test: /\.js$/,
+		        loader: 'babel-loader',
+		        exclude: /node_modules/,
+		        query:{
+		        	presets: ['react','es2015']
+		        }
+			},
+			{
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            }
+		]
+	},
+}
+module.exports = config;
