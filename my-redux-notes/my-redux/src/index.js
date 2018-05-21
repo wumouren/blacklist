3a1b4b07af0263c92e2118ebf03eab94e9a230a4
wuyonghui <18211923525@163.com>
2018-05-21 22:50:40 +0800

    img

--- my-redux-notes/my-redux/src/index.js
+++ my-redux-notes/my-redux/src/index.js
@@ -11,13 +11,26 @@ const store = {
 
 function renderHead (){
   const head = document.getElementById('head')
-  head.innerText = store.head.text;
+  head.innerText = store.head.text + '--' + store.body.text;
+  dispatch({type: 'BODY_TEXT', text: '我是 head 经过调用 dispatch 修改后的 body'})
   head.style.color = store.head.color;
 }
 function renderBody (){
   const body = document.getElementById('body')
   body.innerText = store.body.text;
   body.style.color = store.body.color;
+}
+function dispatch(action) {
+  switch (action.type) {
+    case 'HEAD_COLOR':
+      store.head.color = action.color
+      break
+    case 'BODY_TEXT':
+      store.body.text = action.text
+      break
+    default:
+      break
+  }
 }
 function renderApp (){
   renderHead();
