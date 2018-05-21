f8ac31332f8dbf64558498ceba67713e6724c5d4
wuyonghui <18211923525@163.com>
2018-05-22 00:46:31 +0800

    img

--- my-redux-notes/my-redux/src/index.js
+++ my-redux-notes/my-redux/src/index.js
@@ -1,13 +1,8 @@
-const store = {
-  head: {
-    text: '我是头部',
-    color: 'red'
-  },
-  body: {
-    text: '我是body',
-    color: 'green'
-  }
-}
+import { state } from './state/state.js';
+import { stateChange } from './state/stateChange.js';
+import { createStore } from './redux/createStore.js';
+const { store, dispatch } = createStore(state, stateChange)
+  
 function renderHead (){
   const head = document.getElementById('head')
   head.innerText = store.head.text;
@@ -18,22 +13,11 @@ function renderBody (){
   body.innerText = store.body.text;
   body.style.color = store.body.color;
 }
-function dispatch(action) {
-  switch (action.type) {
-    case 'HEAD_COLOR':
-      store.head.color = action.color
-      break
-    case 'BODY_TEXT':
-      store.body.text = action.text
-      break
-    default:
-      break
-  }
-}
+
 function renderApp (){
   renderHead();
   renderBody();
 }
 renderApp();
-dispatch({type: 'BODY_TEXT', text: '我是经过调用 dispatch 修改后的 body'})
-renderApp();
+dispatch({ type: 'BODY_TEXT', text: '我是调用 dispatch 修改的 body' })
+console.log("store==",store)
